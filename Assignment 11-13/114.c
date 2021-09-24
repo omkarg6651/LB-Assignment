b2079f5d4ba3000437600721d97a83582e6c933b
omkarg6651 <91307686+omkarg6651@users.noreply.github.com>
2021-09-24 16:31:28 +0530

    Add files via upload

---
+++ Assignment 11-13/114.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void Pattern(int,int);
+
+int main()
+{
+	int iValue1 = 0,iValue2 = 0;
+	
+	printf("Enter rows and columns ");
+	scanf("%d%d",&iValue1,&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	return 0;
+}
+void Pattern(int iRow,int iCol)
+{
+	int i = 0,j=0;
+	
+	for(i=iRow;i>0;i--)
+	{
+		for(j=iCol;j>0;j--)
+		{
+			printf("%d\t",i);
+		}
+		printf("\n");
+	}
+}
